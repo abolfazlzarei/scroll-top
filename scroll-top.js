95e76bb76d94e20d1060b9a66d95427179fa4d37
abolfazlzarei <aboli138400@gmail.com>
2023-11-09 10:25:06 +0330

    first commit

---
+++ scroll-top.js
@@ -0,0 +1,24 @@
+let basicScrollTop = function () {
+    let btnTop = document.querySelector('#goTop');
+    let btnReveal = function () {
+
+        if (window.scrollY >= 300) {
+            btnTop.classList.add('is-visible');
+        } else {
+            btnTop.classList.remove('is-visible');
+        }
+    }
+
+    let TopscrollTo = function () {
+        if (window.scrollY != 0) {
+            setTimeout(function () {
+                window.scrollTo(0, window.scrollY - 30);
+                TopscrollTo();
+            }, 6);
+        }
+    }
+    window.addEventListener('scroll', btnReveal);
+    btnTop.addEventListener('click', TopscrollTo);
+
+};
+basicScrollTop();
